0bea6d0036a02f9257c030808bdf094bb4a36ee9
Dmitry <lbvf50.mobile@gmail.com>
2018-03-29 14:35:39 +0700

    Listing 30a: Reporting failure to the console suing $stderr.puts.

---
+++ list30a.rb
@@ -0,0 +1,4 @@
+puts "Ups something wrong happens"
+$stderr.puts "Waiting..."
+sleep 1
+puts "Done"
---
+++ list30a.sh
@@ -0,0 +1,5 @@
+echo "Run ruby without flags"
+ruby list30a.rb
+echo "Run ruby with -W0 flag"
+ruby -W0 list30a.rb
+echo 'No effects on $sterr.puts'

f81cd676efd356e61d96385c957f204ee3ec8e75
Dmitry <lbvf50.mobile@gmail.com>
2018-03-29 14:42:43 +0700

    Listing 30b: Reporting failure to the console (warn, $stderr.puts, ruby -wlevel=0)

---
+++ list30b.rb
@@ -0,0 +1,4 @@
+puts "Something happens I going to inform using warn."
+warn "warn: An error hapens."
+$stderr.puts '$stderr.puts: An error happens.'
+puts "done."
---
+++ list30b.sh
@@ -0,0 +1,4 @@
+echo "-- Call 'warn' example using ruby without keys."
+ruby list30b.rb
+echo "-- Call 'warn' example using ruby -wlevel=0 key"
+ruby -wlevel=0 list30b.rb
